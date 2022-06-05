44dfc47e252ee3e1c0cce3a75f0cc2f1e8cb92fa
lucjan25 <lucjan25.t@gmail.com>
2022-06-05 21:01:16 +0200

    Add project files.

---
+++ PracownikBiblioteka/PracownikClass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PracownikBiblioteka
+{
+    public interface IPracownik
+    {
+        public void Add(IPracownik p);
+        public string WyswietlInformacje();
+        public int Count();
+    }
+    public class Pracownik : IPracownik
+    {
+        private string name;
+        public Pracownik(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void Add(IPracownik p)
+        {
+            return;
+        }
+        public string WyswietlInformacje()
+        {
+            return GetName();
+        }
+        public int Count()
+        {
+            return 1;
+        }
+    }
+    public class Kierownik : IPracownik, IEnumerable<IPracownik>
+    {
+        private List<IPracownik> pracownicy = new List<IPracownik>();
+        private string name;
+        public Kierownik(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void Add(IPracownik p)
+        {
+            pracownicy.Add(p);
+        }
+        public string WyswietlInformacje()
+        {
+            int i = 0;
+            string result = GetName() + ":(";
+            foreach (IPracownik p in pracownicy)
+            {
+                result += p.WyswietlInformacje();
+                if (i != pracownicy.Count - 1)
+                {
+                    result += " + ";
+                }
+                i++;
+            }
+            return result + ")";
+        }
+
+        public int Count()
+        {
+            int count = 1;
+            foreach (IPracownik p in pracownicy)
+            {
+                count += p.Count();
+            }
+            return count;
+        }
+
+        public IEnumerator<IPracownik> GetEnumerator()
+        {
+            return pracownicy.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return pracownicy.GetEnumerator();
+        }
+    }
+}
---
+++ PracownikTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using PracownikBiblioteka;
+
+namespace PracownikTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kierownik kierownik_top = new("Główny kierownik");
+            Kierownik kierownik_1 = new("drugi kierownik");
+            Kierownik kierownik_2 = new("trzeci kierownik");
+            Pracownik pracownik_1 = new("pracownik1");
+            Pracownik pracownik_2 = new("pracownik2");
+            Pracownik pracownik_3 = new("pracownik3");
+            Pracownik pracownik_4 = new("pracownik4");
+
+            kierownik_top.Add(kierownik_1);
+            kierownik_1.Add(pracownik_1);
+            kierownik_top.Add(pracownik_2);
+            kierownik_1.Add(kierownik_2);
+            kierownik_2.Add(pracownik_3);
+            kierownik_2.Add(pracownik_4);
+
+            Console.WriteLine("Struktura: " + kierownik_top.WyswietlInformacje());
+            Console.WriteLine("Count: " + kierownik_top.Count().ToString());
+        }
+    }
+}
